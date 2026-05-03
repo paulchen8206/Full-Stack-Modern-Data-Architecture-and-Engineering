01dcefdfb195c0ee6e0f49aa587ec54b9e258050
paulchen8206 <70728547+paulchen8206@users.noreply.github.com>
2026-05-02 20:41:51 -0700

    helm code review

--- cicd/scripts/build-images.sh
+++ cicd/scripts/build-images.sh
@@ -8,22 +8,25 @@ PROCESSOR_IMAGE="${PROCESSOR_IMAGE:-pos-processor:0.1.0}"
 CONNECT_IMAGE="${CONNECT_IMAGE:-pos-connect:0.1.0}"
 DBT_IMAGE="${DBT_IMAGE:-pos-dbt:0.1.0}"
 AIRFLOW_IMAGE="${AIRFLOW_IMAGE:-pos-airflow:0.1.0}"
+ICEBERG_WRITER_IMAGE="${ICEBERG_WRITER_IMAGE:-pos-iceberg-writer:0.1.0}"
 mdm_cdc_curate_IMAGE="${mdm_cdc_curate_IMAGE:-pos-mdm-cdc-curate:0.1.0}"
 mdm_rds_pg_IMAGE="${mdm_rds_pg_IMAGE:-pos-mdm-pyspark-sync:0.1.0}"
 
-docker build -t "${PRODUCER_IMAGE}" ./source-apps/ods_source
-docker build -t "${PROCESSOR_IMAGE}" ./process-apps/ods_processor
+docker build -t "${PRODUCER_IMAGE}" ./source-apps/ods-source
+docker build -t "${PROCESSOR_IMAGE}" ./process-apps/ods-processor
 docker build -t "${CONNECT_IMAGE}" ./kafka-connect/ods-connect
 docker build -t "${DBT_IMAGE}" ./analytics/dbt
 docker build -t "${AIRFLOW_IMAGE}" ./platform-services/airflow
+docker build -t "${ICEBERG_WRITER_IMAGE}" ./process-apps/iceberg-writer
 docker build -t "${mdm_cdc_curate_IMAGE}" ./process-apps/mdm-cdc-curate
-docker build -t "${mdm_rds_pg_IMAGE}" ./process-apps/mdm-pyspark-sync
+docker build -t "${mdm_rds_pg_IMAGE}" ./process-apps/mdm-rds-pg
 
 kind load docker-image --name "${CLUSTER_NAME}" "${PRODUCER_IMAGE}"
 kind load docker-image --name "${CLUSTER_NAME}" "${PROCESSOR_IMAGE}"
 kind load docker-image --name "${CLUSTER_NAME}" "${CONNECT_IMAGE}"
 kind load docker-image --name "${CLUSTER_NAME}" "${DBT_IMAGE}"
 kind load docker-image --name "${CLUSTER_NAME}" "${AIRFLOW_IMAGE}"
+kind load docker-image --name "${CLUSTER_NAME}" "${ICEBERG_WRITER_IMAGE}"
 kind load docker-image --name "${CLUSTER_NAME}" "${mdm_cdc_curate_IMAGE}"
 kind load docker-image --name "${CLUSTER_NAME}" "${mdm_rds_pg_IMAGE}"
 
